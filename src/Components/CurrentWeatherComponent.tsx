fcbed85920a516af474a0fae22e43cf9055d6de6
ChristmasLover12345 <madnes0205@gmail.com>
2025-03-20 16:10:30 -0700

    getting there with the logic

--- src/Components/CurrentWeatherComponent.tsx
+++ src/Components/CurrentWeatherComponent.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
-const CurrentWeatherComponent = () => {
+const CurrentWeatherComponent = (props: { icon: string; temp: number; minTemp: number; maxTemp: number }) => {
   return (
     <div className="bg-[#D9D9D9]/40 backdrop-invert backdrop-opacity-10 rounded-[5px] flex flex-col items-center justify-self-center md:w-[55%] md:row-start-4 md:row-end-7 md:col-start-1 md:col-end-5">
     <h1 className="text-white text-center text-[50px] ">Current Weather</h1>
     <div className="w-full h-[78%] flex flex-row">
-      <img className="h-[90%] w-[45%] self-center" src="/images/king-von-rapper.gif" alt="Weather Icon" />
+      <img className="h-[90%] w-[45%] self-center" src={props.icon} alt="Weather Icon" />
       <div className="flex flex-col items-center justify-center h-full w-[55%]">
-        <h1 className="text-center text-[75px]">152*</h1>
+        <h1 className="text-center text-[75px]">{`${props.temp}°C`}</h1>
         <div className="flex flex-row justify-around w-full">
           <div className="flex flex-col">
             <h3 className="text-black text-center text-[25px]">Min</h3>
             <h3 className="text-black text-center text-[25px]">temperature</h3>
-            <h3 className="text-center text-[25px]">132*</h3>
+            <h3 className="text-center text-[25px]">{`${props.minTemp}°C`}</h3>
           </div>
           <div className="flex flex-col">
             <h3 className="text-black text-center text-[25px]">Max</h3>
             <h3 className="text-black text-center text-[25px]">temperature</h3>
-            <h3 className="text-center text-[25px]">132*</h3>
+            <h3 className="text-center text-[25px]">{`${props.maxTemp}°C`}</h3>
           </div>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import CurrentWeatherComponent from "@/Components/CurrentWeatherComponent";
 import FavoritesComponent from "@/Components/FavoritesComponent";
 import FutureWeatherComponent from "@/Components/FutureWeatherComponent";
 import InfoDisplayComponent from "@/Components/InfoDisplayComponent";
-import { CurrentWeather, getName } from "@/Services/DataFetches";
+import { CurrentWeather, forecastWeather, getName } from "@/Services/DataFetches";
 import { removeFromLocalSeen, saveToLocalSeen } from "@/Services/LocalStorage";
 import { useState } from "react";
 
@@ -23,6 +23,29 @@ export default function Home() {
   const [day, setDay] = useState("")
   const [month, setMonth] = useState("")
   const [year, setYear] = useState(0)
+
+  const [minTemp, setMinTemp] = useState(0)
+  const [maxTemp, setMaxTemp] = useState(0)
+  const [currentIcon, setCurrentIcon] = useState("")
+  const [currentTemp, setCurrentTemp] = useState(0)
+
+  const [futureMinTemp1, setFutureMinTemp1] = useState("")
+  const [futureMinTemp2, setFutureMinTemp2] = useState("")
+  const [futureMinTemp3, setFutureMinTemp3] = useState("")
+  const [futureMinTemp4, setFutureMinTemp4] = useState("")
+  const [futureMinTemp5, setFutureMinTemp5] = useState("")
+
+  const [futureMaxTemp1, setFutureMaxTemp1] = useState("")
+  const [futureMaxTemp2, setFutureMaxTemp2] = useState("")
+  const [futureMaxTemp3, setFutureMaxTemp3] = useState("")
+  const [futureMaxTemp4, setFutureMaxTemp4] = useState("")
+  const [futureMaxTemp5, setFutureMaxTemp5] = useState("")
+
+  const[futureIcon1, setFutureIcon1] = useState("")
+  const[futureIcon2, setFutureIcon2] = useState("")
+  const[futureIcon3, setFutureIcon3] = useState("")
+  const[futureIcon4, setFutureIcon4] = useState("")
+  const[futureIcon5, setFutureIcon5] = useState("")
 
   const getCityName = async () => {
     let data = await getName(userSearch)
@@ -43,7 +66,7 @@ export default function Home() {
       getCityName()
       .then(() => {
       currentDisplay() 
-      // forecastFunc()
+      forecastFunc()
       
       })
       }
@@ -68,7 +91,32 @@ export default function Home() {
     setYear(currentDate.getFullYear());
 
 
+    // Setting the current weather
+    setCurrentIcon(`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    setCurrentTemp(data.main.temp);
+    setMinTemp(data.main.temp_min);
+    setMaxTemp(data.main.temp_max);
+
+    setAdd2Fav(cityName)
+
   };
+
+  const forecastFunc = async () => {
+    const data = await forecastWeather(lat, lon)
+
+    setFutureMinTemp1(((data.list[0].main.temp_min + data.list[1].main.temp_min + data.list[2].main.temp_min + data.list[3].main.temp_min + data.list[4].main.temp_min + data.list[5].main.temp_min + data.list[6].main.temp_min + data.list[7].main.temp_min) / 8).toFixed())
+    setFutureMinTemp2(((data.list[8].main.temp_min + data.list[9].main.temp_min + data.list[10].main.temp_min + data.list[11].main.temp_min + data.list[12].main.temp_min + data.list[13].main.temp_min + data.list[14].main.temp_min + data.list[15].main.temp_min) / 8).toFixed())
+    setFutureMinTemp3(((data.list[16].main.temp_min + data.list[17].main.temp_min + data.list[18].main.temp_min + data.list[19].main.temp_min + data.list[20].main.temp_min + data.list[21].main.temp_min + data.list[22].main.temp_min + data.list[23].main.temp_min) / 8).toFixed())
+    setFutureMinTemp4(((data.list[24].main.temp_min + data.list[25].main.temp_min + data.list[26].main.temp_min + data.list[27].main.temp_min + data.list[28].main.temp_min + data.list[29].main.temp_min + data.list[30].main.temp_min + data.list[31].main.temp_min) / 8).toFixed())
+    setFutureMinTemp5(((data.list[32].main.temp_min + data.list[33].main.temp_min + data.list[34].main.temp_min + data.list[35].main.temp_min + data.list[36].main.temp_min + data.list[37].main.temp_min + data.list[38].main.temp_min + data.list[39].main.temp_min) / 8).toFixed())
+
+    setFutureMaxTemp1(((data.list[0].main.temp_max + data.list[1].main.temp_max + data.list[2].main.temp_max + data.list[3].main.temp_max + data.list[4].main.temp_max + data.list[5].main.temp_max + data.list[6].main.temp_max + data.list[7].main.temp_max) / 8).toFixed())
+    setFutureMaxTemp2(((data.list[8].main.temp_max + data.list[9].main.temp_max + data.list[10].main.temp_max + data.list[11].main.temp_max + data.list[12].main.temp_max + data.list[13].main.temp_max + data.list[14].main.temp_max + data.list[15].main.temp_max) / 8).toFixed())
+    setFutureMaxTemp3(((data.list[16].main.temp_max + data.list[17].main.temp_max + data.list[18].main.temp_max + data.list[19].main.temp_max + data.list[20].main.temp_max + data.list[21].main.temp_max + data.list[22].main.temp_max + data.list[23].main.temp_max) / 8).toFixed())
+    setFutureMaxTemp4(((data.list[24].main.temp_max + data.list[25].main.temp_max + data.list[26].main.temp_max + data.list[27].main.temp_max + data.list[28].main.temp_max + data.list[29].main.temp_max + data.list[30].main.temp_max + data.list[31].main.temp_max) / 8).toFixed())
+    setFutureMaxTemp5(((data.list[32].main.temp_max + data.list[33].main.temp_max + data.list[34].main.temp_max + data.list[35].main.temp_max + data.list[36].main.temp_max + data.list[37].main.temp_max + data.list[38].main.temp_max + data.list[39].main.temp_max) / 8).toFixed())
+
+  }
 
   return (
     <div className="h-screen w-screen bg-[url(/images/weatherdevice.jpg)] flex justify-center items-center bg-cover bg-center bg-no-repeat">
@@ -85,7 +133,7 @@ export default function Home() {
        <InfoDisplayComponent day={day} month={month} year={year} placeName={cityName}/>
 
           {/* current weather display */}
-          <CurrentWeatherComponent/>
+          <CurrentWeatherComponent icon={currentIcon} temp={currentTemp} minTemp={minTemp} maxTemp={maxTemp}/>
 
           <FavoritesComponent />
 
